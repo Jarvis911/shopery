33e28a2e02e0865ffe677e2d89f6f015aa29c0b5
TrongTin <letrongtin1012@gmail.com>
2025-08-11 22:34:51 +0700

    update 11/8

--- js/11_product_details.js
+++ js/11_product_details.js
@@ -1,68 +1,76 @@
 
-    let quantities = JSON.parse(localStorage.getItem('quantities')) ||
-    {
-      quantity: 1,
-      count: 0,
-      total: 0
-    };
+let quantities = JSON.parse(localStorage.getItem('quantities')) ||
+{
+  quantity: 1,
+  count: 0,
+  total: 0
+};
 
-    document.getElementById('cart-total').textContent = '$' + quantities.total.toFixed(2);
-    document.getElementById('cart-count').textContent = quantities.count;
+document.getElementById('cart-total').textContent = '$' + quantities.total.toFixed(2);
+document.getElementById('cart-count').textContent = quantities.count;
 
-    function changequantity(amount) {
-      quantities.quantity += amount;
-      if (quantities.quantity < 1) {
-        quantities.quantity = 1;
-      }
-      document.getElementById('quantity').textContent = quantities.quantity
-
-
-    }
-    function addtocardt() {
-      quantities.count += quantities.quantity;
-      quantities.total = quantities.count * 17.28;
-      document.getElementById('cart-count').textContent = quantities.count;
-      document.getElementById('cart-total').textContent = '$' + quantities.total.toFixed(2);
-      quantities.quantity = 1;
-      document.getElementById('quantity').textContent = quantities.quantity;
-      localStorage.setItem('quantities', JSON.stringify(quantities));
+function changequantity(amount) {
+  quantities.quantity += amount;
+  if (quantities.quantity < 1) {
+    quantities.quantity = 1;
+  }
+  document.getElementById('quantity').textContent = quantities.quantity
 
 
-    }
-
-    function fillheart(hearid) {
-      const heartSvg = document.getElementById(hearid);
-      heartSvg.classList.toggle('active');
-
-      /*if (heartSvg.classList.contains('active')) {
-    heartSvg.classList.remove('active');
-  } else {
-    heartSvg.classList.add('active');
-  }*/
-    }
+}
+function addtocardt() {
+  quantities.count += quantities.quantity;
+  quantities.total = quantities.count * 17.28;
+  document.getElementById('cart-count').textContent = quantities.count;
+  document.getElementById('cart-total').textContent = '$' + quantities.total.toFixed(2);
+  quantities.quantity = 1;
+  document.getElementById('quantity').textContent = quantities.quantity;
+  localStorage.setItem('quantities', JSON.stringify(quantities));
 
 
-    function changeimg(thumbnail) {
-      let mainthumbnail = document.getElementById("main_thumbnail")
-      mainthumbnail.src = thumbnail.src;
-      
-      let thumbnails = document.querySelectorAll(".thumbnail");
-      thumbnails.forEach(img => img.classList.remove("active"));
-      thumbnail.classList.add("active");
+}
 
-    }
+function fillheart(hearid) {
+  const heartSvg = document.getElementById(hearid);
+  heartSvg.classList.toggle('active');
 
-    function changetab(tabid) {
-      document.querySelectorAll(".tabcontent").forEach(tabcontent => tabcontent.classList.remove("active"));
-      document.querySelectorAll(".tab").forEach(tab => tab.classList.remove("active"));
-      document.getElementById(tabid).classList.add("active");
-      let tabchange = [...document.querySelectorAll(".tab")].find(tab => tab.textContent.replace(/\s/g, '').toLowerCase().includes(tabid));
-      tabchange.classList.add("active");
-      if (tabid === 'feedback') {
-        document.querySelector(".product-detail_description_body_right").classList.remove('active');
-      }
-      else {
-        document.querySelector(".product-detail_description_body_right").classList.add('active');
-      }
-    }
+  /*if (heartSvg.classList.contains('active')) {
+heartSvg.classList.remove('active');
+} else {
+heartSvg.classList.add('active');
+}*/
+}
+
+
+function changeimg(thumbnail) {
+  let mainthumbnail = document.getElementById("main_thumbnail")
+  mainthumbnail.src = thumbnail.src;
+
+  let thumbnails = document.querySelectorAll(".thumbnail");
+  thumbnails.forEach(img => img.classList.remove("active"));
+  thumbnail.classList.add("active");
+
+}
+
+function changetab(tabid) {
+  document.querySelectorAll(".tabcontent").forEach(tabcontent => tabcontent.classList.remove("active"));
+  document.querySelectorAll(".tab").forEach(tab => tab.classList.remove("active"));
+  document.getElementById(tabid).classList.add("active");
+  let tabchange = [...document.querySelectorAll(".tab")].find(tab => tab.textContent.replace(/\s/g, '').toLowerCase().includes(tabid));
+  tabchange.classList.add("active");
+  if (tabid === 'feedback') {
+    document.querySelector(".product-detail_description_body_right").classList.remove('active');
+  }
+  else {
+    document.querySelector(".product-detail_description_body_right").classList.add('active');
+  }
+}
+
+function changecolor(logo) 
+{
+document.querySelectorAll(".social").forEach(social => social.classList.remove("active"));
+  document.querySelectorAll(".background").forEach(background => background.classList.remove("active"));
   
+  logo.querySelector(".social").classList.add("active");
+  logo.querySelector(".background").classList.add("active");
+}
