22dd07dc2fb64ea291f2af94cefaa278bedeaa9e
TrongTin <letrongtin1012@gmail.com>
2025-08-22 19:32:24 +0700

    update 22/8

--- js/check-out.js
+++ js/check-out.js
@@ -6,3 +6,25 @@ document.querySelectorAll('input[type="radio"]').forEach(radio => {
       this.wasChecked = this.checked;
     });
   });
+
+   document.getElementById("billingForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    
+    let data = {
+      firstName: document.getElementById("firstName").value,
+      lastName: document.getElementById("lastName").value,
+      company: document.getElementById("company").value,
+      address: document.getElementById("address").value,
+      country: document.getElementById("country").value,
+      state: document.getElementById("state").value,
+      zip: document.getElementById("zip").value,
+      email: document.getElementById("email").value,
+      phone: document.getElementById("phone").value
+    };
+
+    localStorage.setItem("billingInfo", JSON.stringify(data));
+    alert("Thông tin đã được lưu vào localStorage!");
+    
+
+    this.reset();  
+  });
